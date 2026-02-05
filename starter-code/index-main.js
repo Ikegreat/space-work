a58ea3593cf90f9e539931fdd451fedb9b1d722b
unknown <ikechukwuchiemerie822@gmail.com>
2026-02-05 11:45:22 +0100

    first commit

---
+++ starter-code/index-main.js
@@ -0,0 +1,18 @@
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn");
+const parentMenu = document.getElementById("parentMenu")
+
+openBtn.addEventListener("click", () => {
+    parentMenu.classList.add("active");
+    openBtn.style.display = "none";
+  closeBtn.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+    parentMenu.classList.remove("active")
+     closeBtn.style.display = "none";
+  openBtn.style.display = "block";
+});
+
+
+
